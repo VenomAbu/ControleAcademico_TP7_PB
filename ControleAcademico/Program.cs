c9fc00c3d9ff30bf39f2ed4154fe1a19657446a9
Danillo Liz Mendes Mailan Silva <danillo.silva@al.infnet.edu.br>
2024-03-30 20:38:11 -0300

    Adicionar arquivos de projeto.

---
+++ ControleAcademico/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControleAcademico
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Disciplina disciplina = new Disciplina(1, "Java");
+            Professor professor = new Professor(1, "Elberth");
+            Turma turma = new Turma(1, disciplina);
+
+            Aluno aluno1 = new Aluno(1, "Mario");
+            Aluno aluno2 = new Aluno(2, "Luigi");
+            Aluno aluno3 = new Aluno(3, "Wario");
+
+            Console.WriteLine(turma.addAluno(aluno1));
+            Console.WriteLine(turma.addAluno(aluno2));
+            Console.WriteLine(turma.addAluno(aluno3));
+
+            Console.WriteLine(turma.gerarPauta());
+
+            Console.WriteLine(aluno1);
+            Console.WriteLine(professor);
+            Console.WriteLine(turma);
+        }
+
+    }
+}
